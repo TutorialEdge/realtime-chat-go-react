1b082b8ea924e18877041a7781bf4e935c069f07
unnatikala05 <53338821+unnatikala05@users.noreply.github.com>
2019-10-01 12:33:21 +0530

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import 'react' from 'react-redux';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
